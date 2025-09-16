d3a9595e65aa42294354c31d29cbe7378acc7067
Captain Moron <132834512+BunThangDacHieu@users.noreply.github.com>
2025-09-16 17:30:56 +0700

    Điều chỉnh angular

--- inventory/src/app/features/warehouse/services/warehouse.service.ts
+++ inventory/src/app/features/warehouse/services/warehouse.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Warehouse } from '../../../shared/model/warehouse';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WarehouseService {
+  private baseUrl: string = 'http://localhost:8080';
+  private warehouseUrl: string = `${this.baseUrl}/warehouse`;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllWarehouse() {
+    return this.http.get<Warehouse[]>(this.baseUrl + `/warehouse`);
+  }
+
+  getWarehouseById(id: number) {
+    return this.http.get<Warehouse[]>(this.baseUrl + `/warehouse/${id}`);
+  }
+
+  createWarehouse(warehouse: any) {
+    return this.http.post<Warehouse[]>(this.baseUrl + `/warehouse`, warehouse);
+  }
+
+  updateWarehouse(id: number, warehouse: Warehouse) {
+    return this.http.put<Warehouse[]>(
+      this.baseUrl + `/warehouse/${id}`,
+      warehouse
+    );
+  }
+
+  deleteWarehouse(id: number) {
+    return this.http.delete(this.baseUrl + `/warehouse/${id}`);
+  }
 }
